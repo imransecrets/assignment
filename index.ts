0311d93853bd8b87c0126c28b0d09ebf6345d26a
asif imran <imransecrets@gmail.com>
2023-07-21 20:16:45 +0500

    This is my 1st assignment

---
+++ index.ts
@@ -0,0 +1,19 @@
+/* My Particular */
+console.log("\n");
+console.log("Student Name : Asif Imran"); // my name
+console.log("Roll No: PIAIC223565"); // my Roll No in PIAIC Porgram
+console.log("\n");
+
+/*1.	Install Node.js, TypeScript and VS Code on your computer. */
+console.log ("q1 Answer :");
+console.log ("Sir Node.js and Typscript installed  with all extensions and working on it.");
+console.log("\n");
+
+/*2.	Personal Message: Store a person’s name in a variable, and 
+print a message to that person. Your message should be simple, such
+as, “Hello Eric, would you like to learn some Python today?” */
+console.log ("q2 Answer :");
+let personName: string= "Eric";//declaring String 
+let personMessage: string = "Hello "+ personName + ", would you like to learn some Python today.";// declarign string
+console.log(personMessage);
+console.log("\n");
