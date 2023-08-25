65e109443d1e5cfb07a940d85a06edd840250f42
imransecrets <imransecrets@gmail.com>
2023-08-24 19:20:24 -0700

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-/* My Particular */
-console.log("\n");
-console.log("Student Name : Asif Imran"); // my name
-console.log("Roll No: PIAIC223565"); // my Roll No in PIAIC Porgram
-console.log("\n");
-
